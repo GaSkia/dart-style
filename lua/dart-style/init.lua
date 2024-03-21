da5a8583c9d51ea6d81c0bb34e2aa5e6b6771747
gaskia <gabry.schiavo00@gmail.com>
2024-03-21 20:15:42 +0000

    update 3

--- lua/dart-style/init.lua
+++ lua/dart-style/init.lua
@@ -5,6 +5,9 @@ function M.SetTabstop()
 
     if filetype == "dart" then
         vim.cmd("setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab")
+    else
+        -- Default tabstop settings
+        vim.cmd("setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab")
     end
 end
 
